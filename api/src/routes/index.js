6ae92f59abc35f9ceb7eb283aa4bd4e61917da51
Franco <mansilla.franco.1@gmail.com>
2024-07-31 15:06:23 -0300

    cree rutas services

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const userRoute = require('./userRoute');
+const serviceRoute = require('./serviceRoute');
 
 const router = Router();
 
-router.use('/user', userRoute);
+router.use('/users', userRoute);
+router.use('/services', serviceRoute);
 
 
 module.exports = router;
