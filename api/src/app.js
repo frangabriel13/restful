7333390fb4191ee976d07eb709d0663eacb382b7
Franco <mansilla.franco.1@gmail.com>
2024-08-09 17:46:42 -0300

    canda

--- api/src/app.js
+++ api/src/app.js
@@ -22,7 +22,7 @@ app.use(passport.initialize());
 // app.use(passport.session());
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:5173',
   credentials: true,
   optionsSuccessStatus: 200
 };
