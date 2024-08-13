920a2e73d7fa126cb3f4fb09c3203aae4ed10674
Franco <mansilla.franco.1@gmail.com>
2024-08-13 17:55:43 -0300

    cambios

---
+++ client/src/utils/utilities.js
@@ -0,0 +1,13 @@
+import one from "../assets/benefits/1.jpg";
+import two from "../assets/benefits/2.jpg";
+import three from "../assets/benefits/3.jpg";
+import four from "../assets/benefits/4.jpg";
+import five from "../assets/benefits/5.jpg";
+
+export const benfitsImages = [
+  one,
+  two,
+  three,
+  four,
+  five
+];
