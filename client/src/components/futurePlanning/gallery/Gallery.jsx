c8a707b44b65f1f58de6c5910c69a234520d17c8
Franco <mansilla.franco.1@gmail.com>
2024-08-13 17:13:20 -0300

    cambios

---
+++ client/src/components/futurePlanning/gallery/Gallery.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from "./Gallery.module.css";
+
+const Gallery = () => {
+  return (
+    <div className={s.container}>
+      <h3>Galería de imágenes</h3>
+    </div>
+  )
+};
+
+
+export default Gallery;
