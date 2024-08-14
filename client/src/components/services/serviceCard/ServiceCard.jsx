21229875868976629ad05c4ee79d0ece4b0ff76f
Franco <mansilla.franco.1@gmail.com>
2024-08-14 18:11:52 -0300

    cambios

--- client/src/components/services/serviceCard/ServiceCard.jsx
+++ client/src/components/services/serviceCard/ServiceCard.jsx
@@ -7,7 +7,7 @@ const ServiceCard = ({ service }) => {
     <div className={s.card}>
       {
         servicesImages && (
-          <div>
+          <div className={s.divImg}>
             <img 
               src={servicesImages.find(img => img.id === service.id)?.image} 
               alt={service.name} 
@@ -15,9 +15,26 @@ const ServiceCard = ({ service }) => {
           </div>
         )
       }
-      <div className={s.container}>
-        <h3>{service.name}</h3>
-        <p>{service.description}</p>
+      <div className={s.divDataContainer}>
+        <div className={s.dataContainer}>
+          <div className={s.divData}>
+            <h3>{service.name}</h3>
+            <p>{service.description}</p>
+          </div>
+          <div className={s.divFeatures}>
+            <h4>Beneficios:</h4>
+            <ul>
+              {
+                service.features.map((feature, index) => (
+                  <li key={index}>{feature}</li>
+                ))
+              }
+            </ul>
+          </div>
+        </div>
+        <div className={s.divMoreInfo}>
+          <button>Más información</button>
+        </div>
       </div>
     </div>
   );
