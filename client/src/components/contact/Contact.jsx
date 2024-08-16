cdcbc0ed065b6e10a7a545c2f4eaaa8a55c93ab4
Franco <mansilla.franco.1@gmail.com>
2024-08-16 17:42:21 -0300

    cambios

--- client/src/components/contact/Contact.jsx
+++ client/src/components/contact/Contact.jsx
@@ -5,98 +5,15 @@ import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/style.css'
 import { sendEmail } from "../../redux/actions/emailActions";
 import { validateForm } from "../../utils/validations";
+import ContactForm from "./contactForm/ContactForm";
 
 const Contact = () => {
-  const dispatch = useDispatch();
-  const [formData, setFormData] = useState({
-    name: "",
-    lastName: "",
-    age: "",
-    type: "",
-    phone: "",
-    email: "",
-  });
-  const [errors, setErrors] = useState({});
-  console.log(formData);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  // const handlePhoneChange = (value) => {
-  //   setFormData({ ...formData, phone: value });
-  // };
-  const handlePhoneChange = (value, country, e, formattedValue) => {
-    setFormData({ ...formData, phone: formattedValue });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const validationErrors = validateForm(formData);
-    if(Object.keys(validationErrors).length > 0) {
-      setErrors(validationErrors);
-      return;
-    }
-    const { email, name, lastName, type, phone, age } = formData;
-    const subject = `Consulta de ${name} ${lastName}`;
-    const text = `
-      Nombre: ${name} ${lastName}
-      Email: ${email}
-      Teléfono: ${phone}
-      Tipo de consulta: ${type}
-      Edad: ${age}
-    `;
-    console.log(text);
-    dispatch(sendEmail(email, subject, text));
-    setFormData({
-      name: "",
-      lastName: "",
-      age: "",
-      type: "",
-      phone: "",
-      email: "",
-    });
-    setErrors({});
-  };
-
   return (
     <div className={s.container}>
-      <div className={s.divForm}>
-        <h2>Necesitas asesoría inmediata?</h2>
-        <form onSubmit={handleSubmit}>
-          <div className={s.divInput}>
-            <input type="text" name="name" placeholder="Nombre" className={s.input} value={formData.name} onChange={handleChange} />
-            {errors.name && <p className={s.error}>{errors.name}</p>}
-            <input type="text" name="lastName" placeholder="Apellido" className={s.input} value={formData.lastName} onChange={handleChange} />
-          </div>
-          <div className={s.divInput}>
-            <input type="number" name="age" placeholder="Edad" className={s.input} value={formData.age} onChange={handleChange} />
-            <select name="type" className={s.select} value={formData.type} onChange={handleChange} >
-              <option value="value1">Consulta</option>
-              <option value="value2">Asesoría</option>
-              <option value="value3">Otro</option>
-            </select>
-          </div>
-          <div className={s.divPhone}>
-            <PhoneInput
-              country={'us'}
-              value={formData.phone}
-              placeholder="Teléfono"
-              containerClass={s.phoneInputContainer}
-              inputClass={s.phoneInput}
-              buttonClass={s.phoneInputButton}
-              dropdownClass={s.phoneInputDropdown}
-              searchClass={s.phoneInputSearch}
-              onChange={handlePhoneChange}
-            />
-            <input type="email" name="email" placeholder="Email" className={s.input} value={formData.email} onChange={handleChange} />
-          </div>
-          <div className={s.divSend}>
-            <button type="submit" className={s.btnSend}>Enviar</button>
-          </div>
-        </form>
+      <div className={s.divTitle}>
+        <h2>Iniciar arreglos</h2>
       </div>
+      <ContactForm />
     </div>
   );
 }
--- client/src/components/contact/contactForm/ContactForm.jsx
+++ client/src/components/contact/contactForm/ContactForm.jsx
@@ -1,30 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import s from "./ContactForm.module.css";
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
+import { sendEmail } from "../../../redux/actions/emailActions";
+import { validateForm } from "../../../utils/validations";
+import { getServices } from "../../../redux/actions/serviceActions";
+import { useDispatch, useSelector } from "react-redux";
 
 const ContactForm = () => {
+  const dispatch = useDispatch();
+  const services = useSelector((state) => state.service.services);
+  const [form, setForm] = useState({
+    name: "",
+    lastname: "",
+    age: "",
+    phone: "",
+    service: "",
+  });
+
+  useEffect(() => {
+    dispatch(getServices());
+  }, []);
+
+  console.log(services);
+
   return (
-    <div>
-      <div>
-        <h4>Completa el formulario para recibir asesoramiento</h4>
+    <div className={s.container}>
+      <div className={s.divTitle}>
+        <h3>Completa el formulario para recibir asesoramiento</h3>
         <p>Te responderemos a la brevedad</p>
       </div>
-      <div>
+      <div className={s.divForm}>
         <form>
-          <div>
-            <label>Nombre</label>
-            <input type="text" />
+          <div className={s.divName}>
+            <div>
+              <input type="text" name="name" placeholder="Nombre" />
+            </div>
+            <div>
+              <input type="text" name="lastname" placeholder="Apellido" />
+            </div>
           </div>
-          <div>
-            <label>Correo electrónico</label>
-            <input type="email" />
-          </div>
-          <div>
-            <label>Teléfono</label>
-            <input type="tel" />
-          </div>
-          <div>
-            <label>Mensaje</label>
-            <textarea></textarea>
+          <div className={s.services}>
+            <div>
+              <input type="number" name="age" placeholder="Edad" />
+            </div>
+            <div className={s.service}>
+              <select name="service">
+                <option value="">Selecciona un servicio</option>
+                {services.map((service) => (
+                  <option key={service.id} value={service.id}>
+                    {service.name}
+                  </option>
+                ))}
+                <option value="not_sure">No estoy seguro (a)</option>
+              </select>
+            </div>
           </div>
           <button>Enviar</button>
         </form>
