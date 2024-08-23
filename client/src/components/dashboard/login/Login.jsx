751a6c93c1cc609dd473035e54503905884404c8
Franco <mansilla.franco.1@gmail.com>
2024-08-23 13:11:23 -0300

    cambios

---
+++ client/src/components/dashboard/login/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import s from "./Login.module.css";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className={s.container}>
+      <h1>Login</h1>
+    </div>
+  );
+};
+
+
+export default Login;
