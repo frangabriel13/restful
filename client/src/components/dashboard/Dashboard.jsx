2bef2a1e24ca589b00b727b5d43a310298af369e
Franco <mansilla.franco.1@gmail.com>
2024-08-24 15:59:53 -0300

    cambios

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import s from "./Dashboard.module.css";
 import SideBar from "./sideBar/SideBar";
 import DashService from "./dashService/DashService";
+import Management from "./management/Management";
 
 const Dashboard = () => {
   return (
@@ -11,6 +12,7 @@ const Dashboard = () => {
       <div className={s.dashboard}>
         <Routes>
           <Route path="services" element={<DashService />} />
+          <Route path="orders" element={<Management />} />
         </Routes>
       </div>
     </div>
