61ba72492276e8179bb36df4a7fc317bb414ecb0
Franco <mansilla.franco.1@gmail.com>
2024-09-09 12:40:46 -0300

    camvbios

---
+++ client/src/components/dashboard/users/Users.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import s from "./Users.module.css";
+import { useDispatch, useSelector } from "react-redux";
+// import { getUsers, deleteUser } from "../../../redux/actions/userActions";
+
+const Users = () => {
+  return (
+    <div>
+      <h2>Users</h2>
+    </div>
+  )
+};
+
+
+export default Users;
