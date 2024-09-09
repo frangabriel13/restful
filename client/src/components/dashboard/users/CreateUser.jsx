fcf5c12cc7ce28ecc213a10180287a0cdc363907
Franco <mansilla.franco.1@gmail.com>
2024-09-09 15:58:28 -0300

    cambios

---
+++ client/src/components/dashboard/users/CreateUser.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import s from "./CreateUser.module.css";
+// import { createUser } from "../../../redux/actions/userActions";
+// import { validateCreateUser } from "../../../utils/validations";
+
+const CreateUser = ({ handleCancel }) => {
+  return (
+    <div>
+      <h2>Crear Usuario</h2>
+    </div>
+  )
+};
+
+
+export default CreateUser;
--- client/src/components/dashboard/users/Users.jsx
+++ client/src/components/dashboard/users/Users.jsx
@@ -1,12 +1,85 @@
 import React, { useEffect, useState } from "react";
 import s from "./Users.module.css";
 import { useDispatch, useSelector } from "react-redux";
-// import { getUsers, deleteUser } from "../../../redux/actions/userActions";
+import { getUsers, deleteUser } from "../../../redux/actions/userActions";
+import EditUser from "./EditUser";
+import CreateUser from "./CreateUser";
 
 const Users = () => {
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.user.users);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [showEdit, setShowEdit] = useState(false);
+  const [showCreate, setShowCreate] = useState(false);
+
+  console.log(users);
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+
+  const handleEdit = (user) => {
+    setSelectedUser(user);
+    setShowEdit(true);
+  };
+
+  const handleCreate = () => {
+    setShowCreate(true);
+  };
+
+  const handleCancel = () => {
+    setSelectedUser(null);
+    setShowEdit(false);
+  };
+
+  const handleDelete = (id) => {
+    dispatch(deleteUser(id));
+  };
+
   return (
-    <div>
-      <h2>Users</h2>
+    <div className={s.container}>
+      <div className={s.dashboard}>
+        <h2>Funeral Homes</h2>
+        <table className={s.table}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Role</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.role}</td>
+                <td className={s.btnCell}>
+                  <button onClick={() => handleEdit(user)}>Edit</button>
+                  <button className={s.btnDelete} onClick={() => handleDelete(user.id)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className={s.btnCreateContainer}>
+          <button className={s.btnCreate} onClick={handleCreate}>Create</button>
+        </div>
+      </div>
+      {
+        showEdit && (
+          <EditUser
+            funeralHome={selectedUser}
+            handleCancel={handleCancel}
+          />
+        )
+      }
+      {
+        showCreate && (
+          <CreateUser handleCancel={() => setShowCreate(false)} />
+        )
+      }
     </div>
   )
 };
