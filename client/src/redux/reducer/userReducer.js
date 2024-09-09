5a55366ab156383bae46a5a65490929c2d676bad
Franco <mansilla.franco.1@gmail.com>
2024-09-09 13:27:13 -0300

    cambios

---
+++ client/src/redux/reducer/userReducer.js
@@ -0,0 +1,25 @@
+const initialState = {
+  user: null,
+  // isAuthenticated: false,
+  // token: null,
+};
+
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'GET_USERS':
+      return {
+        ...state,
+        users: action.payload,
+      };
+    case 'DELETE_USER':
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
+
+
+export default userReducer;
