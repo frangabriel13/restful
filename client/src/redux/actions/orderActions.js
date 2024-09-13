b9518bcdfd0fb7529af4ffc405cd6c572c83e315
Franco <mansilla.franco.1@gmail.com>
2024-09-13 12:59:36 -0300

    cambiops

---
+++ client/src/redux/actions/orderActions.js
@@ -0,0 +1,49 @@
+import { instance } from "../../utils/axiosConfig";
+
+export const getOrders = () => async (dispatch) => {
+  try {
+    const response = await instance.get("orders");
+    dispatch({
+      type: "GET_ORDERS",
+      payload: response.data,
+    });
+  } catch(error) {
+    console.error(error);
+  }
+};
+
+export const updateOrder = (id, order) => async (dispatch) => {
+  try {
+    const response = await instance.put(`orders/${id}`, order);
+    dispatch({
+      type: "UPDATE_ORDER",
+      payload: response.data,
+    });
+  } catch(error) {
+    console.error(error);
+  }
+};
+
+export const createOrder = (order) => async (dispatch) => {
+  try {
+    const response = await instance.post("orders", order);
+    dispatch({
+      type: "CREATE_ORDER",
+      payload: response.data,
+    });
+  } catch(error) {
+    console.error(error);
+  }
+};
+
+export const deleteOrder = (id) => async (dispatch) => {
+  try {
+    await instance.delete(`orders/${id}`);
+    dispatch({
+      type: "DELETE_ORDER",
+      payload: id,
+    });
+  } catch(error) {
+    console.error(error);
+  }
+};
