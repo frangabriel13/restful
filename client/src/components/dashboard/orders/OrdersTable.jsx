f8cdb6043077342a7fd6decacf50d4931992eeb6
Franco <mansilla.franco.1@gmail.com>
2024-09-14 19:12:54 -0300

    cambios

--- client/src/components/dashboard/orders/OrdersTable.jsx
+++ client/src/components/dashboard/orders/OrdersTable.jsx
@@ -1,81 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import s from "./OrdersTable.module.css";
-// import { getOrders } from "../../../redux/actions/orderActions";
-
-// const OrdersTable = () => {
-//   const dispatch = useDispatch();
-//   const orders = useSelector((state) => state.order.orders);
-
-//   useEffect(() => {
-//     dispatch(getOrders());
-//   }, [dispatch]);
-
-//   console.log(orders);
-
-//   const formatDate = (dateString) => {
-//     return dateString.split('T')[0];
-//   };
-
-//   return (
-//     <div className={s.dashboard}>
-//       <h2>Orders</h2>
-//       <table className={s.table}>
-//         <thead>
-//           <tr>
-//             <th>Status</th>
-//             <th>Contact Day</th>
-//             <th>Insurance</th>
-//             <th>Funeral Home</th>
-//             <th>Tracking</th>
-//             <th>Price</th>
-//             <th>Contact Name</th>
-//             <th>Phone Number</th>
-//             <th>Email</th>
-//             <th>Relationship</th>
-//             <th>Service Type</th>
-//             <th>Deceased Name</th>
-//             <th>Age</th>
-//             <th>Source</th>
-//             <th>Assigned</th>
-//             <th>Comission</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {orders.map((order) => (
-//             <tr key={order.id}>
-//               <td>{order.status}</td>
-//               <td>{formatDate(order.statusDate.date)} by {order.statusDate.updatedBy}</td>
-//               <td>{order.insurance}</td>
-//               <td>{order.funeralHome ? order.funeralHome.name : 'N/A'}</td>
-//               <td>
-//                 <ul>
-//                   {order.tracking.map((track, index) => (
-//                     <li key={index}>{track}</li>
-//                   ))}
-//                 </ul>
-//               </td>
-//               <td>{order.price}</td>
-//               <td>{order.contactName}</td>
-//               <td>{order.phoneNumber}</td>
-//               <td>{order.email}</td>
-//               <td>{order.relationship}</td>
-//               <td>{order.service ? order.service.name : 'N/A'}</td>
-//               <td>{order.deceasedName}</td>
-//               <td>{order.age}</td>
-//               <td>{order.source}</td>
-//               <td>{order.user ? order.user.name : 'N/A'}</td>
-//               <td>{order.comission.join(", ")}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   )
-// };
-
-
-// export default OrdersTable;
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import s from "./OrdersTable.module.css";
@@ -83,6 +5,9 @@ import { getOrders } from "../../../redux/actions/orderActions";
 import { getFuneralHomes } from "../../../redux/actions/funeralHomeActions";
 import { getServices } from "../../../redux/actions/serviceActions";
 import { getUsers } from "../../../redux/actions/userActions";
+import EditOrder from "./EditOrder";
+import { CiEdit } from "react-icons/ci";
+import { MdDeleteOutline } from "react-icons/md";
 
 const OrdersTable = () => {
   const dispatch = useDispatch();
@@ -123,7 +48,7 @@ const OrdersTable = () => {
       <table className={s.table}>
         <thead>
           <tr>
-            <th>Status</th>
+          <th>Status</th>
             <th>Contact Day</th>
             <th>Insurance</th>
             <th>Funeral Home</th>
@@ -133,25 +58,46 @@ const OrdersTable = () => {
             <th>Phone Number</th>
             <th>Email</th>
             <th>Relationship</th>
-            <th>Service</th>
-            <th>Admin</th>
+            <th>Service Type</th>
+            <th>Deceased Name</th>
+            <th>Age</th>
+            <th>Source</th>
+            <th>Assigned</th>
+            <th>Comission</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {orders.map((order) => (
             <tr key={order.id}>
               <td>{order.status}</td>
-              <td>{formatDate(order.createdAt)}</td>
+              <td>{formatDate(order.statusDate.date)} by {order.statusDate.updatedBy}</td>
               <td>{order.insurance}</td>
               <td>{getFuneralHomeName(order.funeralHomeId)}</td>
-              <td>{order.tracking.join(", ")}</td>
+              <td>
+                <ul>
+                  {order.tracking.map((track, index) => (
+                    <li key={index}>{track}</li>
+                  ))}
+                </ul>
+               </td>
               <td>{order.price}</td>
               <td>{order.contactName}</td>
               <td>{order.phoneNumber}</td>
               <td>{order.email}</td>
               <td>{order.relationship}</td>
               <td>{getServiceName(order.serviceId)}</td>
+              <td>{order.deceasedName}</td>
+              <td>{order.age}</td>
+              <td>{order.source}</td>
               <td>{getUserName(order.userId)}</td>
+              <td>{order.comission.join(", ")}</td>
+              <td>
+                <div className={s.divIcons}>
+                  <CiEdit className={s.iconEdit} />
+                  <MdDeleteOutline className={s.iconDelete} />
+                </div>
+              </td>
             </tr>
           ))}
         </tbody>
