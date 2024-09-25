d71b0e496617642c7780f58c608692fb07e9cfb0
Franco <mansilla.franco.1@gmail.com>
2024-09-25 17:53:01 -0300

    cambios

--- client/src/components/dashboard/orders/modals/ExcelModal.jsx
+++ client/src/components/dashboard/orders/modals/ExcelModal.jsx
@@ -1,10 +1,60 @@
-import React from "react";
+// import React from "react";
+// import s from "./ExcelModal.module.css";
+
+// const ExcelModal = ({ onClose, onImport }) => {
+//   const handleClickOutside = (e) => {
+//     if (e.target === e.currentTarget) {
+//       onClose();
+//     }
+//   };
+
+//   const handleImport = () => {
+//     const data = {};
+//     onImport(data);
+//     // onClose();
+//   }
+
+//   return (
+//     <div className={s.modal} onClick={handleClickOutside}>
+//       <div className={s.modalContent}>
+//         <button onClick={onClose} className={s.btnClose}>Close</button>
+//         <div className={s.container}>
+//           <h3>Excel Import</h3>
+//           <div className={s.form}>
+//             <input type="file" />
+//             <button onClick={handleImport}>Import</button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// };
+
+
+// export default ExcelModal;
+
+import React, { useState } from "react";
 import s from "./ExcelModal.module.css";
 
 const ExcelModal = ({ onClose, onImport }) => {
+  const [file, setFile] = useState(null);
+
   const handleClickOutside = (e) => {
     if (e.target === e.currentTarget) {
       onClose();
+    }
+  };
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleImport = () => {
+    if (file) {
+      onImport(file);
+      onClose();
+    } else {
+      console.error("No file selected");
     }
   };
 
@@ -14,10 +64,14 @@ const ExcelModal = ({ onClose, onImport }) => {
         <button onClick={onClose} className={s.btnClose}>Close</button>
         <div className={s.container}>
           <h3>Excel Import</h3>
+          <div className={s.form}>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleImport}>Import</button>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 
