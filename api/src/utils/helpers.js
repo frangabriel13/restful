2d50a713f995371810a0cd676df3c7c43b115f85
Franco <mansilla.franco.1@gmail.com>
2024-09-26 16:05:10 -0300

    cambios

--- api/src/utils/helpers.js
+++ api/src/utils/helpers.js
@@ -10,7 +10,23 @@ const mapStatus = (excelStatus) => {
   return statusExcel[excelStatus] || 'pending';
 };
 
+const parseTracking = (tracking) => {
+  if (!tracking) return [];
+
+  // Asegurarse de que tracking sea una cadena
+  const trackingString = String(tracking);
+
+  return trackingString.split('/').map(entry => {
+    const comment = entry.trim();
+    return {
+      date: new Date(),
+      track: comment || 'No comment'
+    };
+  });
+};
+
 
 module.exports = {
   mapStatus,
+  parseTracking,
 };
